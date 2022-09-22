93b8d225639737d2ab36490941ce57b525ff4eb4
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2022-09-22 13:18:51 +0300

    добавил путь к папке ресурсов

--- UI.cs
+++ UI.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 using SFML.Graphics;
+using System.Diagnostics;
 
 namespace game_cannons
 {
@@ -14,21 +16,20 @@ namespace game_cannons
     /// </summary>
     internal static class UI
     {
-        static Texture image = new Texture("resources/test_image.png");
+        static string resourcesPath = Environment.CurrentDirectory;
+        //static Texture image = new Texture("resources/test_image.png");
+
+        static UI()
+        {
+            resourcesPath = resourcesPath.Substring(0, resourcesPath.Length - "bin\\Debug\\net6.0".Length);
+        }
+
         /// <summary>
         /// Главный метод класса UI отвечает за отрисовку графики на экран
         /// </summary>
         public static void Draw()
         {
             
-            Sprite sp = new Sprite(image);
-            Sprite sp1 = new Sprite(image);
-            sp.Rotation = 30f;
-            App.window.Draw(sp);
-            App.window.Draw(sp1);
-
-            sp.Dispose();
-            sp1.Dispose();
         }
     }
 }
